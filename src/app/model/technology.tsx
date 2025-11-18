7954cd0f16d2d84d92f095a6c2873b584afc3adc
Nino Cabernard <cabernin@students.zhaw.ch>
2025-11-18 18:15:32 +0100

    json files filled up with data

--- src/app/model/technology.tsx
+++ src/app/model/technology.tsx
@@ -2,13 +2,4 @@ export class Technology {
   name!: string;
   description: string | null = null;
   experience: number | null = null;
-  imgUrl: string | undefined;
-  epxertiseLevel: ExpertiseLevel | undefined;
-}
-
-export enum ExpertiseLevel {
-  Beginner = "Beginner",
-  Intermediate = "Intermediate",
-  Advanced = "Advanced",
-  Expert = "Expert",
 }
