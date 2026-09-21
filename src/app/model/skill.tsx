4fdbdb1d7cc9918220855c917a0c6383295fa546
Nino Cabernard <nino.cabernard@outlook.com>
2026-09-21 17:12:04 +0200

    skill added

--- src/app/model/skill.tsx
+++ src/app/model/skill.tsx
@@ -1,7 +1,12 @@
-import { ExpertiseLevel } from "./technology";
-
 export class Skill {
   name!: string;
   description: string | undefined;
   expertiseLevel: ExpertiseLevel = ExpertiseLevel.Beginner;
+}
+
+export enum ExpertiseLevel {
+  Beginner,
+  Intermediate,
+  Advanced,
+  Expert,
 }
